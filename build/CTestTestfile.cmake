b629c06bd35f0ea1bc9fc293d8fd7bca95264c66
Dxc114514 <Dxc_123456@126.com>
2024-05-04 04:07:08 +0800

    cmake init

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/xanchengd/Test_Repository
+# Build directory: /home/xanchengd/Test_Repository/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
